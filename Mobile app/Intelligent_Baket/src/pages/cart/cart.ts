9ca3b16798d1fe0beb8a99a18d7d47c4ce6c560e
Ajay kushwaha <ajaykushwaha1215@gmail.com>
2018-09-06 21:54:04 +0530

    started with cart

--- Mobile app/Intelligent_Baket/src/pages/cart/cart.ts
+++ Mobile app/Intelligent_Baket/src/pages/cart/cart.ts
@@ -1,25 +1,35 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CartPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-cart',
   templateUrl: 'cart.html',
 })
 export class CartPage {
 
+  ItemsInCart: string[];	
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	this.initializeItems();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CartPage');
+  initializeItems()
+  {
+  	this.ItemsInCart = [
+      'Amsterdam',
+      'Bogota',
+      'cata',
+      'data',
+      'maggie',
+      'water'
+    ];
   }
-
+ 
+  removeItems(ev: any) {
+     var closebtns = document.getElementById(ev.target.id);
+     var str = closebtns.parentElement.textContent;
+     str = str.substring(0,str.length - 1);
+     //console.log(str);
+    this.ItemsInCart = this.ItemsInCart.filter(item => item !== str);
+    }  
 }
