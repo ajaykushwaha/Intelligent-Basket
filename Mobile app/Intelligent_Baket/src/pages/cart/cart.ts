259cd14ce4787760fbc028d666ee7fe1ebe055da
Ajay kushwaha <ajaykushwaha1215@gmail.com>
2018-09-08 13:21:34 +0530

    Added total and pay

--- Mobile app/Intelligent_Baket/src/pages/cart/cart.ts
+++ Mobile app/Intelligent_Baket/src/pages/cart/cart.ts
@@ -1,35 +1,40 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {  NavController, NavParams } from 'ionic-angular';
+
 
 
 @Component({
   selector: 'page-cart',
   templateUrl: 'cart.html',
 })
+
+
 export class CartPage {
 
-  ItemsInCart: string[];	
+  ItemsInCart: string[][];	
+  cart : string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   	this.initializeItems();
   }
 
+
   initializeItems()
   {
   	this.ItemsInCart = [
-      'Amsterdam',
-      'Bogota',
-      'cata',
-      'data',
-      'maggie',
-      'water'
+     ['water','2litre','45'],
+     ['onion','4kg','200'],
+     ['potato','4kg','200'],
+     ['yadayada','2kg','150'],
+     ['pista','5kg','400'],
+     ['guitar','1-piece','7500']
     ];
+    this.cart = "unbilled";
   }
  
   removeItems(ev: any) {
-     var closebtns = document.getElementById(ev.target.id);
-     var str = closebtns.parentElement.textContent;
-     str = str.substring(0,str.length - 1);
-     //console.log(str);
-    this.ItemsInCart = this.ItemsInCart.filter(item => item !== str);
+     var str = ev.target.id;
+     let x  = str.split(",")
+     console.log(x[0]);
+     this.ItemsInCart = this.ItemsInCart.filter(item =>item[0]!==x[0] && item[1]!==x[1] && item[2]!==x[2]);
     }  
 }
